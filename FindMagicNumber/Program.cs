a7a3dae390afe23f8a85b5305410de7a4ee3b3ed
ksistof1986 <ksistof1986@gmail.com>
2020-01-05 22:53:06 +0200

    User Input number convertion to array

--- FindMagicNumber/Program.cs
+++ FindMagicNumber/Program.cs
@@ -10,10 +10,36 @@ namespace FindMagicNumber
     {
         static void Main()
         {
-            //initial project start
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input 6 digit number");
+            int numberInput;
+            numberInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is " +numberInput);
 
+           //1. Convertion to array
+            Console.WriteLine("Converted input to array");
+            // test display
+            for (int i = 0; i <= ConvertToArray(numberInput).Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //2. Check if every element in array ConvertToArray(numberInput) is unique
+
+            
+      
             Console.ReadKey();
+        }
+        static void ConvertToArray2(int numberInput)
+        {
+            
+        }
+
+        //static int ConvertToArray(int numberInput)
+        public static int[] ConvertToArray(int numberInput)
+        {
+
+            int[] digits = new int[6];
+            digits = numberInput.ToString().ToCharArray().Select(Convert.ToInt32).ToArray();
+            return digits;
         }
     }
 }
